2c91efa9dc1eca5c3af4090c02bf546f17402543
Daniella Rocha <darochap@uamv.edu.ni>
2025-04-04 07:42:34 -0600

    Delete Semana 3/Ejercicio3.py

--- Semana 3/Ejercicio3.py
+++
@@ -1,9 +0,0 @@
-salario_bruto = float(input("Ingrese el salario bruto del empleado: "))
-
-impuesto_renta = salario_bruto * 0.10
-
-seguro_social = salario_bruto * 0.05
-
-fondo_pensiones = salario_bruto * 0.03
-
-descuentos = impuesto_renta + seguro_social 
