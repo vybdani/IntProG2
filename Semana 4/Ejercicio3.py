cc2bda78b3600fdace9b3fb1a1d14d61819181ea
Daniella Rocha <darochap@uamv.edu.ni>
2025-04-08 09:13:59 -0600

    Ejercicios 1ra sesion

---
+++ Semana 4/Ejercicio3.py
@@ -0,0 +1,11 @@
+numero1 = 25
+numero2 = 5
+
+print(f"{numero1} > {numero2} el resultado es {numero1 > numero2}")
+print(f"{numero1} < {numero2} el resultado es {numero1 > numero2}")
+print(f"{numero1} >= {numero2} el resultado es {numero1 > numero2}")
+print(f"{numero1} <= {numero2} el resultado es {numero1 > numero2}")
+print(f"{numero1} > {numero2} el resultado es {not(numero1 > numero2)}")
+print(f"{numero1} < {numero2} el resultado es {not(numero1 > numero2)}")
+print(f"{numero1} >= {numero2} el resultado es {not(numero1 > numero2)}")
+print(f"{numero1} <= {numero2} el resultado es {not(numero1 > numero2)}")
