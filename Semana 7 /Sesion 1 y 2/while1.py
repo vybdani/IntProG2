afc6725a33068837c64eb1e140a89d5c97e1ea93
Daniella Rocha <darochap@uamv.edu.ni>
2025-05-06 09:52:57 -0600

    Delete Semana 7 /Sesion 1 y 2/while1.py

--- Semana 7 /Sesion 1 y 2/while1.py
+++
@@ -1,8 +0,0 @@
-"""Leer una palabra diferente a fin y convertirla a mayuscula"""
-
-palabra = input("Dime una palabra: ")
-while palabra.lower()!= "fin":
-    print(f"{palabra.upper()} tiene {len(palabra)} letras")
-    palabra = input("Dime una palabra: ")
-print("Adios...")
-    
