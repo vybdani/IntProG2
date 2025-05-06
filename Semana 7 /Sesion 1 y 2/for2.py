85694f9b20423a048562e1edeb7178f256d593c0
Daniella Rocha <darochap@uamv.edu.ni>
2025-05-06 09:51:59 -0600

    Semana 7

--- Semana 7 /Sesion 1 y 2/for2.py
+++
@@ -1,6 +0,0 @@
-"""Leer una palabra difernete a fin y convertirla a mayuscula"""
-
-for palabra in iter(input, "fin"):
-    print(f"{palabra.capitalize()} tiene {len(palabra)} letras")
-else: 
-    print("Termino...")
