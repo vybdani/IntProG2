7361664addae54604423d273391c7ef4250bac6b
Daniella Rocha <darochap@uamv.edu.ni>
2025-06-07 10:57:37 -0600

    Create Funciones2.py

---
+++ Semana 11/Sesion 1 y 2/Funciones2.py
@@ -0,0 +1,7 @@
+def sumar(numero1, numero2):
+    return numero1 + numero2
+
+n1 = int(input("Ingrese el primer número: "))
+n2 = int(input("Ingrese el segundo número: "))
+sumar = sumar(n1, n2)
+print(f"La suma de {n1} y {n2} es: {sumar}")
