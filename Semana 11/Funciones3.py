7c61fc46fa51ab64f34d3c8aad29e00f7f645eb9
Daniella Rocha <darochap@uamv.edu.ni>
2025-06-07 10:58:29 -0600

    Semana 11

--- Semana 11/Funciones3.py
+++
@@ -1,8 +0,0 @@
-#Calcular el area de un circulo
-
-def calcular_area_circulo(radio):
-    pi = 3.14159
-    return pi * (radio ** 2)
-radio = float(input("Ingrese el radio del círculo: "))
-area = calcular_area_circulo(radio)
-print(f"El área del círculo con radio {radio} es: {area:.2f}") 
