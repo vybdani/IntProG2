d51708576561b42d506c75a77621aa6b1aea00dc
Daniella Rocha <darochap@uamv.edu.ni>
2025-06-10 06:56:16 -0600

    Semana 11

---
+++ Semana 11/Sesion 3/Ejercicio 2/models/estudiante.py
@@ -0,0 +1,8 @@
+
+class Estudiante:
+    def __init__(self, nombres, apellidos, carrera, anio, promedio):
+        self.nombres = nombres
+        self.apellidos = apellidos
+        self.carrera = carrera
+        self.anio = anio
+        self.promedio = promedio
